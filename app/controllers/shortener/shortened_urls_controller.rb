06e03ab0caa4834b39fd08d8640306961b34c13f
Adrian CB <abarbul@gmail.com>
2017-06-26 12:02:12 +1000

    Permitting controller params.

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -4,9 +4,16 @@ class Shortener::ShortenedUrlsController < ActionController::Base
   include Shortener
 
   def show
-    token = ::Shortener::ShortenedUrl.extract_token(params[:id])
+    token = ::Shortener::ShortenedUrl.extract_token(shortener_params[:id])
     track = Shortener.ignore_robots.blank? || request.human?
-    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params, track: track)
+    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: shortener_params, track: track)
     redirect_to url[:url], status: :moved_permanently
+  end
+
+  private
+
+  # ugh...
+  def shortener_params
+    params.permit(:id, :controller, :action)
   end
 end
